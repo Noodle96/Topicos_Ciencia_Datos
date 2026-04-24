4b4d141cb8fd1ec23018edfc12ea7470968bca3e
Noodle96 <jorgealfredomarino2@gmail.com>
2026-04-24 10:56:40 -0500

    data science life cycle,update files

--- Data_Science_Life_Cycle/utilidades.py
+++ Data_Science_Life_Cycle/utilidades.py
@@ -1,14 +1,27 @@
+import pandas as pd
+
+
 def saludar(name):
     """
     Función que imprime un saludo personalizado.
-    
+
     Parameters:
     name (str): El nombre de la persona a saludar.
-    
+
     Returns:
     None
     """
     print(f"Hola, {name}! Bienvenido de")
 
-def contarRegistrosConmayusculas(columna):
-    return sum(columna.str.contains(r'[A-Z]', na=False))
+
+def contarRegistrosConmayusculas(columna: pd.Series) -> int:
+    """
+    Count how many entries in a pandas Series contain uppercase letters.
+
+    Args:
+        columna (pd.Series): Column of strings.
+
+    Returns:
+        int: Number of entries containing uppercase letters.
+    """
+    return int(columna.str.contains(r"[A-Z]", na=False).sum())
